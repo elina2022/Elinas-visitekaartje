92ecbb477692f151efc65583aaf55668560e214c
elina2022 <elinakniazian@gmail.com>
2022-09-10 18:38:33 +0200

    buttons
    buttons are now clickable

---
+++ script.js
@@ -0,0 +1,23 @@
+function test(){
+    document.getElementById("changingtext").innerHTML = "elinakniazian@gmail.com";
+}
+
+function test2(){
+    document.getElementById("changingtext").innerHTML = "06-11388334";
+}
+
+function test3(){
+    document.getElementById("changingtext").innerHTML = "I like to...";
+}
+
+function test4(){
+    document.getElementById("changingtext").innerHTML = "My qualities";
+}
+
+function test5(){
+    document.getElementById("changingtext").innerHTML = "Things i value";
+}
+
+function test6(){
+    document.getElementById("changingtext").innerHTML = "Some things on my never ending bucketlist";
+}
