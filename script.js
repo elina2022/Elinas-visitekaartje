030719a46c74347069d9d5d6fd4e666bfc349d90
elina2022 <elinakniazian@gmail.com>
2022-09-11 20:31:42 +0200

    idk

--- script.js
+++ script.js
@@ -15,9 +15,13 @@ function test4(){
 }
 
 function test5(){
-    document.getElementById("changingtext").innerHTML = "Things i value...<br>alone time<br>health<br>learning new things<br>nature";
+    document.getElementById("changingtext").innerHTML = "Things I value...<br>alone time<br>health<br>learning new things<br>nature";
 }
 
 function test6(){
     document.getElementById("changingtext").innerHTML = "Some things on my never ending bucketlist...<br> jump out of a plane (...with a parachute!)<br>finish a triathlon<br>stand under a waterfall<br>learn to speak Russian";
+}
+
+function test7(){
+    document.getElementById("changingtext").innerHTML = "18 years • Armenian ethnicity<br>1st year FDND<br>HTML, CSS and JavaScript ★☆☆☆☆<br><i>'watch a sunrise at least once a day'</i><br>Also... try clicking my face ;)";
 }
