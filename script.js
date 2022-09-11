fe3120ed6128411895cd7b401d76be2520cb39e3
elina2022 <elinakniazian@gmail.com>
2022-09-11 12:53:38 +0200

    added text

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function test2(){
 }
 
 function test3(){
-    document.getElementById("changingtext").innerHTML = "I like to...";
+    document.getElementById("changingtext").innerHTML = "Currently I like to...<br>dance<br>draw<br>read<br>bake";
 }
 
 function test4(){
-    document.getElementById("changingtext").innerHTML = "My qualities";
+    document.getElementById("changingtext").innerHTML = "My qualities...<br>ambitious<br>(sometimes too) detail focused<br>creatief<br>some say stubborn, i say strong-willed ;)";
 }
 
 function test5(){
-    document.getElementById("changingtext").innerHTML = "Things i value";
+    document.getElementById("changingtext").innerHTML = "Things i value...<br>alone time<br>health<br>learning new things<br>nature";
 }
 
 function test6(){
-    document.getElementById("changingtext").innerHTML = "Some things on my never ending bucketlist";
+    document.getElementById("changingtext").innerHTML = "Some things on my never ending bucketlist...<br> jump out of a plane (...with a parachute!)<br>finish a triathlon<br>stand under a waterfall<br>learn to speak Russian";
 }
