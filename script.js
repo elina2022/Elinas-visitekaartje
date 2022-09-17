b3312b1cfa4ada1dc7653786679012589d9b7721
elina2022 <elinakniazian@gmail.com>
2022-09-18 01:00:05 +0200

    changes

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-function test(){
+function b1(){
     document.getElementById("changingtext").innerHTML = "elinakniazian@gmail.com";
 }
 
-function test2(){
+function b2(){
     document.getElementById("changingtext").innerHTML = "06-11388334";
 }
 
-function test3(){
+function b3(){
     document.getElementById("changingtext").innerHTML = "Currently I like to...<br>dance<br>draw<br>read<br>bake";
 }
 
-function test4(){
+function b4(){
     document.getElementById("changingtext").innerHTML = "My qualities...<br>ambitious<br>(sometimes too) detail focused<br>creatief<br>some say stubborn, i say strong-willed ;)";
 }
 
-function test5(){
+function b5(){
     document.getElementById("changingtext").innerHTML = "Things I value...<br>alone time<br>health<br>learning new things<br>nature";
 }
 
-function test6(){
+function b6(){
     document.getElementById("changingtext").innerHTML = "Some things on my never ending bucketlist...<br> jump out of a plane (...with a parachute!)<br>finish a triathlon<br>stand under a waterfall<br>learn to speak Russian";
 }
 
-function test7(){
+function b7(){
     document.getElementById("changingtext").innerHTML = "18 years • Armenian ethnicity<br>1st year FDND<br>HTML, CSS and JavaScript ★☆☆☆☆<br><i>'watch a sunrise at least once a day'</i><br>Also... try clicking the picture with color ;)";
 }
